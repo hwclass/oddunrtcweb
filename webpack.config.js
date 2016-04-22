7b94d3ac1b8e8da7d324869c9eeb7e97f3745d52
hwclass <harderworking.class@gmail.com>
2016-04-23 00:26:08 +0300

    added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const autoprefixer = require('autoprefixer'),
+      ExtractTextPlugin = require('extract-text-webpack-plugin'),
+      path = require('path');
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './client/src/stylesheets')
+]
+
+module.exports = {
+  entry: [
+    './client/src/App.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx$|\.js$/,
+        loader: 'eslint-loader',
+        include: __dirname + '/client/**/*',
+        exclude: /bundle\.js$/
+      }
+    ],
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ]
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      },
+      { 
+        test: /\.jsx?$/, 
+        exclude: /(node_modules|bower_components)/, 
+        loader: 'babel' 
+      },
+      { 
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      },
+      { 
+        test: /\.(woff|woff2)$/,
+        loader:"url?prefix=font/&limit=5000" 
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      { 
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      },
+      { 
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        loader: 'url?limit=100000&name=[name].[ext]'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.sass'],
+    modulesDirectories : ['./client/src/stylesheets', './node_modules']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './client',
+    inline : true
+  }
+};
