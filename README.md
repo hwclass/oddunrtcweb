ffd1c231414568020499eaea4cf0476aea3320c2
hwclass <harderworking.class@gmail.com>
2016-04-23 00:20:51 +0300

    added the first component as App, created the directories.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # oddunrtcweb
 The site for oddunrtc.
+
+###Steps
+####Install
+```sudo npm install```
+####Start
+```npm run start-webpack```
---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title : 'ODDUNRTC WEB SITE'
+    }
+  }
+  render() {
+    return(
+      <div className="container">
+        {this.state.title}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
