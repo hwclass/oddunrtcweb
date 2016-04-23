d7658c516a8b97d2c8d8d182a306a8ef930935e1
hwclass <harderworking.class@gmail.com>
2016-04-23 10:12:15 +0300

    added Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <div className="container">
         <Nav/>
+        <Header/>
         {this.state.title}
       </div>
     )
---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  return(
+    <header>
+      <div class="header-content">
+        <div class="header-content-inner">
+          <h1>GERÇEK ZAMANLI VİDEOLU İLETİŞİM</h1>
+          <hr/>
+          <p>Daha iyisi yapılana kadar en iyi WebRTC destekli iletişim aracı.</p>
+          <a href="#about" class="btn btn-primary btn-xl page-scroll">Dahası..</a>
+        </div>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
